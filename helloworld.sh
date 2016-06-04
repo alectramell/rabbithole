2f86e56465ea875efa2a4e64bf16f1c7fa705671
alectramell <alectramell@gmail.com>
2016-06-04 14:25:23 -0700

    rabbithole

---
+++ helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+# Written by Apollon Data Metrics (AGY)
+# https://www.facebook.com/apollondma
+
+clear
+
+USERNAME=$(whoami)
+WHEREAMI=$(pwd)
+
+clear
+
+echo "Hello World!"
+
+sleep 5
+
+clear
